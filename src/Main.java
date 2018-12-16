66ba8acbc33c89842ad5d1eee8705216756f5862
Dahan Schuster <dan.plschuster@gmail.com>
2018-12-15 22:06:31 -0300

    Solved 15/12/2018 daily challenge

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,29 @@ import codesignal.tasks.dahanschuster.*;
 @SuppressWarnings("unused")
 public class Main {
 
+	/**
+	 * Use for testing the tasks. You can copy and paste input examples from the doc
+	 * of each method.
+	 * 
+	 * @TasksAuthor: Dahan Schuster
+	 * @CodeSignalProfile: https://app.codesignal.com/profile/dahan-schust
+	 */
 	public static void main(String[] args) {
-		
-		System.out.println( Arcade_level_03.reverseParentheses("The ((quick (brown) (fox) jumps over the lazy) dog)"));
-		
+
+		String[][] matrix = 
+				{ 
+				{ "o","o","o","o","|","|","|","|"}, 
+				{"|","|","|","|","o","o","o","o"}, 
+				{"-","-","-",".","-","-","-","-"}, 
+				{"o","o","o","o","|","o","o","o"}, 
+				{"|","o","o","o","o","|","o","o"}, 
+				{"o","|","o","o","o","o","|","o"}, 
+				{"o","o","|","o","o","o","o","|"}, 
+				{"o","o","o","|","o","o","o","o" } 
+				};
+
+		System.out.println(DailyChallenges.matrixToDecimal(matrix));
+
 	}
 
 }
