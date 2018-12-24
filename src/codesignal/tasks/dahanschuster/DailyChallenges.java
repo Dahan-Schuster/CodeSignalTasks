5e71de7ea61c181fd4f379d4fa0a25387ddece4e
Dahan Schuster <dan.plschuster@gmail.com>
2018-12-24 11:06:54 -0300

    --amend

--- src/codesignal/tasks/dahanschuster/DailyChallenges.java
+++ src/codesignal/tasks/dahanschuster/DailyChallenges.java
@@ -4,78 +4,7 @@ import java.util.Arrays;
 
 public class DailyChallenges {
 
-	/**
-	 * During the holiday season you go through a lot of candles! You burn many
-	 * candles of various sizes, represented by the array candleSizes.
-	 * 
-	 * You prefer big candles, so you always light the biggest candle available at
-	 * the time.
-	 * 
-	 * For every n candles you burn, you can use the remaining wax to create a new
-	 * candle! The size of this new candle will depend on the sizes of the n candles
-	 * you got the wax from. More specifically, the size of the new candle will be
-	 * equal to the sum of the sizes of the n candles, divided by n + 1 (rounded
-	 * down).
-	 * 
-	 * Given candleSizes and n, your task is to find the sum of the sizes of all
-	 * candles burned!
-	 * 
-	 * Notes:
-	 * 
-	 * If the size of the new candle would be rounded down to zero, there isn't
-	 * enough wax to make a new candle. You can't make a new candle with fewer than
-	 * n leftovers. Example
-	 * 
-	 * For candleSizes = [2, 181, 32, 188, 188] and n = 3, the output should be
-	 * burningCandles(candleSizes, n) = 773.
-	 * 
-	 * There's a tie for biggest candle, so you'll burn one of the 188 ones. The
-	 * total is now 188 and the remaining candles are [2, 181, 32, 188]. You can now
-	 * burn the other 188 candle. The total is now 376 and the remaining candles are
-	 * [2, 181, 32]. The next biggest candle is 181, and since you've now burned 3
-	 * (n) candles, you can make a new one! It will have a size of floor((188 + 188
-	 * + 181) / 4) = 139. So the total is now 557 and the remaining candles are [2,
-	 * 32, 139]. You'll burn the 139 candle next, since it's by far the biggest. The
-	 * total is now 696 and the remaining candles are [2, 32]. The next candle will
-	 * be the 32, so the total is now 728, and only [2] remains. You burn the 2
-	 * next, and it seems like we're all out of candles now, but since you've now
-	 * burned another 3 candles, you can make another new one! This one will have a
-	 * size of floor((139 + 32 + 2) / 4) = 43. So the total is now 730 with [43]
-	 * remaining. You now burn the final candle (43) for a total of 773. Input /
-	 * Output
-	 * 
-	 * [execution time limit] 3 seconds (java)
-	 * 
-	 * [input] array.integer candleSizes
-	 * 
-	 * An array of integers representing the sizes of each of your candles. Not in
-	 * any particular order.
-	 * 
-	 * Guaranteed constraints: 0 ≤ candleSizes.length ≤ 104 1 ≤ candleSizes[i] ≤ 106
-	 * 
-	 * [input] integer n
-	 * 
-	 * An integer representing the number of candles you need to burn in order to be
-	 * able to create a new one from the leftover wax.
-	 * 
-	 * Guaranteed constraints: 2 ≤ n ≤ 104
-	 * 
-	 * [output] integer
-	 * 
-	 * An integer representing the sum of the sizes of all candles burned (including
-	 * candles you created from leftovers).
-	 * 
-	 */
-	public static int burningCandles(int[] candleSizes, int n) {
-		//while (candleSizes.length > 0) {
-			//for ()
-			int[] newCandleSizes = new int[candleSizes.length-1];
-			Arrays.sort(candleSizes);
-			
-		//}
-		return 0;
-		
-	}
+	
 
 	
 
